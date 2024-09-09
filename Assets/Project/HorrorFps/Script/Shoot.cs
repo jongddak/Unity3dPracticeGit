ff73d1c26e5a852218f7bec7cffe6bf2fd9f69fb
jongddak <hfsmsj316@gmail.com>
2024-09-10 00:38:59 +0900

    주석작성
    주석작성

--- Assets/Project/HorrorFps/Script/Shoot.cs
+++ Assets/Project/HorrorFps/Script/Shoot.cs
@@ -15,6 +15,7 @@ public class Shoot : MonoBehaviour
     [SerializeField] GameObject Soundplayer;
 
     [SerializeField] ParticleSystem shootingFire;
+    [SerializeField] TrailRenderer bulletTrail;
 
 
 
@@ -38,6 +39,18 @@ public class Shoot : MonoBehaviour
             AudioSource audio = Soundplayer.GetComponent<AudioSource>();
             audio.PlayOneShot(shoot);
             shootingFire.Play();
+
+            // 예광탄 효과를 만들려면 트레일 렌더러 (진짜 총알을 보내서 이동시켜야함 )
+            // 라인렌더러를 쓰면 힛 한 부분까지 라인렌더링 가능 
+            // 레이캐스트 hit 하면 그 부분에 
+            //void SpawnImpactEffect(RaycastHit hit)
+            //{
+            //    // 1.데칼을 맞은 위치에 생성
+            //    GameObject decal = Instantiate(bulletHoleDecal, hit.point, Quaternion.LookRotation(hit.normal));
+            //    Destroy(decal, 5f); // 5초 후 데칼 제거
+            //}
+            // 2.진짜 총알을 보내서 충돌기준으로(각각의 마테리얼마다 다르게 만들기 가능 )
+            // => 결론 총구에서 레이캐스트해서 힛 한 부분에 데칼 + 라인렌더러 !
         }
     }
 
